fa35c43c27f1d204990cae4c05770a6bb6795ce4
shaysorkin <shaysorkin@gmail.com>
2019-02-21 13:39:52 +0200

    Initial Commit

---
+++ quiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  quiz
+//
+//  Created by Shay Sorkin on 21/02/2019.
+//  Copyright © 2019 Shay Sorkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
